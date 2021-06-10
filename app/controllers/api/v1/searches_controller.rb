5fedc0bd879e5f3daf931bbda75a8e6ba7093330
Joseph Budina <joebud3@gmail.com>
2021-06-10 14:42:03 -0600

    Add conditional to searches

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -1,13 +1,29 @@
 class Api::V1::SearchesController < ApplicationController
 
   def index
-    if params[:origin] && params[:destination] && params[:passengers] && params[:departure] &&  User.find_by(api_key: params[:api_key])
-      user = User.find_by(api_key: params[:api_key])
-      user.searches.create!(origin: params[:origin], destination: params[:destination], passengers: params[:passengers], departure: params[:departure], return_date: params[:returndate], max_price: params[:price], limit: params[:limit])
-      flights = FlightsFacade.flight_object(params[:origin], params[:destination], params[:passengers], params[:departure], params[:returndate], params[:maxprice], params[:limit])
-      render json: FlightsSerializer.new(flights)
+    if required_params &&  User.find_by(api_key: params[:api_key])
+      user = User.find_by(api_key: params[:api_key])  
+      if user.searches.find_by(required_params)
+        user.searches.update(searches_params)
+        flights = FlightsFacade.flight_object(params[:origin], params[:destination], params[:passengers], params[:departure], params[:returndate], params[:maxprice], params[:limit])
+        render json: FlightsSerializer.new(flights)
+      else
+        user.searches.create!(searches_params)
+        flights = FlightsFacade.flight_object(params[:origin], params[:destination], params[:passengers], params[:departure], params[:returndate], params[:maxprice], params[:limit])
+        render json: FlightsSerializer.new(flights)
+      end
     else
       render json: {error: "Invalid Parameters"}, status: 400
     end
+  end
+
+  private
+
+  def required_params
+    params.permit(:origin, :destination, :passengers, :departure)
+  end
+
+  def searches_params
+    params.permit(:origin, :destination, :passengers, :departure, :returndate, :maxprice, :limit)
   end
 end
