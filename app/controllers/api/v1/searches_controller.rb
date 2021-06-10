a77bcb0c070da5435e78d73a1e65294e63b7f4c7
Joseph Budina <joebud3@gmail.com>
2021-06-10 16:50:25 -0600

    Add sort param

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -1,15 +1,34 @@
 class Api::V1::SearchesController < ApplicationController
 
   def index
-    if required_params &&  User.find_by(api_key: params[:api_key])
-      user = User.find_by(api_key: params[:api_key])  
-      if user.searches.find_by(required_params)
-        user.searches.update(searches_params)
-        flights = FlightsFacade.flight_object(params[:origin], params[:destination], params[:passengers], params[:departure], params[:returndate], params[:maxprice], params[:limit])
+    user = User.find_by(api_key: params[:api_key])
+    if required_params && user
+      search = user.searches.find_by(required_params)
+      if search
+        search.update(searches_params)
+        flights = FlightsFacade.flight_object(
+          params[:origin], 
+          params[:destination], 
+          params[:passengers], 
+          params[:departure], 
+          params[:returndate], 
+          params[:maxprice], 
+          params[:limit],
+          params[:sort]
+        )
         render json: FlightsSerializer.new(flights)
       else
-        user.searches.create!(searches_params)
-        flights = FlightsFacade.flight_object(params[:origin], params[:destination], params[:passengers], params[:departure], params[:returndate], params[:maxprice], params[:limit])
+        user.searches.create(searches_params)
+        flights = FlightsFacade.flight_object(
+          params[:origin], 
+          params[:destination], 
+          params[:passengers], 
+          params[:departure], 
+          params[:returndate], 
+          params[:maxprice], 
+          params[:limit],
+          params[:sort]
+        )
         render json: FlightsSerializer.new(flights)
       end
     else
@@ -24,6 +43,6 @@ class Api::V1::SearchesController < ApplicationController
   end
 
   def searches_params
-    params.permit(:origin, :destination, :passengers, :departure, :returndate, :maxprice, :limit)
+    params.permit(:origin, :destination, :passengers, :departure, :returndate, :maxprice, :limit, :sort)
   end
 end
