b45febbb9702edb39b0504de172cb54ccaced4a0
Joseph Budina <joebud3@gmail.com>
2021-06-10 11:09:02 -0600

    Add user auth to index function

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -1,8 +1,9 @@
 class Api::V1::SearchesController < ApplicationController
 
   def index
-    # binding.pry
-    if params[:origin] && params[:destination] && params[:passengers] && params[:departure]
+    if params[:origin] && params[:destination] && params[:passengers] && params[:departure] &&  User.find_by(api_key: params[:api_key])
+      user = User.find_by(api_key: params[:api_key])
+      user.searches.create!(origin: params[:origin], destination: params[:destination], passengers: params[:passengers], departure: params[:departure], return_date: params[:returndate], max_price: params[:price], limit: params[:limit])
       flights = FlightsFacade.flight_object(params[:origin], params[:destination], params[:passengers], params[:departure], params[:returndate], params[:maxprice], params[:limit])
       render json: FlightsSerializer.new(flights)
     else
