5a7fa86caadf82b7fffdca51863c3e52a3d8058f
Joseph Budina <joebud3@gmail.com>
2021-06-10 08:53:12 -0600

    Remove underscores

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::SearchesController < ApplicationController
   def index
     # binding.pry
     if params[:origin] && params[:destination] && params[:passengers] && params[:departure]
-      flights = FlightsFacade.flight_object(params[:origin], params[:destination], params[:passengers], params[:departure], params[:return_date], params[:max_price], params[:limit])
+      flights = FlightsFacade.flight_object(params[:origin], params[:destination], params[:passengers], params[:departure], params[:returndate], params[:maxprice], params[:limit])
       render json: FlightsSerializer.new(flights)
     else
       render json: {error: "Invalid Parameters"}, status: 400
